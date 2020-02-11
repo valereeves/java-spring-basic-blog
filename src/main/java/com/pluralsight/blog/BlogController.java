b8905f978d3aa1ac0e9e390c976380e908e6e3b7
Reeves, Valeri <Valeri.Reeves@T-Mobile.com>
2020-02-11 12:33:00 -0500

    display static data from post repository

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,5 +1,10 @@
 package com.pluralsight.blog;
 
+import com.pluralsight.blog.data.PostRepository;
+import com.pluralsight.blog.model.Post;
+
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,9 +12,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class BlogController {
 
+    private PostRepository postRepository;
+
+    public BlogController(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
     @RequestMapping("/")
     public String listPosts(ModelMap modelMap) {
+        List<Post> posts = postRepository.getAllPosts();
+
         modelMap.put("title", "Blog Post 1");
+        modelMap.put("posts", posts );
         return "home";
     }
 
